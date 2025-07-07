c3a35cc5fc026a6570d13951a0e1f4b6fedf7f95
lunaserrano <48137753+lunaserrano@users.noreply.github.com>
2025-07-06 18:16:03 -0600

    agregando proyecto

---
+++ src/app.routes.ts
@@ -0,0 +1,22 @@
+import { Routes } from '@angular/router';
+import { AppLayout } from './app/layout/component/app.layout';
+// import { Dashboard } from './app/pages/dashboard/dashboard';
+import { Landing } from './app/pages/landing/landing';
+import { Notfound } from './app/pages/notfound/notfound';
+import { MainProductosComponent } from './app/pages/main-productos';
+
+export const appRoutes: Routes = [
+    {
+        path: '',
+        component: AppLayout,
+        children: [
+            { path: '', component: MainProductosComponent },
+            // { path: 'uikit', loadChildren: () => import('./app/pages/uikit/uikit.routes') },
+            { path: 'pages', loadChildren: () => import('./app/pages/pages.routes') }
+        ]
+    },
+    { path: 'landing', component: Landing },
+    { path: 'notfound', component: Notfound },
+    { path: 'auth', loadChildren: () => import('./app/pages/auth/auth.routes') },
+    { path: '**', redirectTo: '/notfound' }
+];
